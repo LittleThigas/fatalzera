806bc3a87b9a9c98422a259457d2b879586c7e2f
LittleThigas <147623461+LittleThigas@users.noreply.github.com>
2023-10-28 00:21:04 -0300

    deletando um file

--- processa_form.php
+++ processa_form.php
@@ -1,17 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $email = $_POST["email"];
 
-    $destinatario = "fatalzerax@gmail.com"; // Substitua pelo seu endereço de email
-    $assunto = "Novo email do formulário do site";
-
-    // Mensagem de email
-    $mensagem = "Email: " . $email;
-
-    // Envia o email
-    mail($destinatario, $assunto, $mensagem);
-
-    // Redireciona para uma página de confirmação
-    header("Location: index.html");
-}
-?>
