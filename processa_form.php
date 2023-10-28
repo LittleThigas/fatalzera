1991e5dae17714aafaa8e7243696bed604546111
LittleThigas <147623461+LittleThigas@users.noreply.github.com>
2023-10-27 23:36:59 -0300

    subir os arquivos do meu site

---
+++ processa_form.php
@@ -0,0 +1,17 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST["email"];
+
+    $destinatario = "seu_email@example.com"; // Substitua pelo seu endereço de email
+    $assunto = "Novo email do formulário do site";
+
+    // Mensagem de email
+    $mensagem = "Email: " . $email;
+
+    // Envia o email
+    mail($destinatario, $assunto, $mensagem);
+
+    // Redireciona para uma página de confirmação
+    header("Location: index.html");
+}
+?>
