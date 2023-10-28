dd9984c02f505dedbe6c2ce3077a0c977df27d30
LittleThigas <147623461+LittleThigas@users.noreply.github.com>
2023-10-27 23:51:21 -0300

    update site

--- processa_form.php
+++ processa_form.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
 
-    $destinatario = "seu_email@example.com"; // Substitua pelo seu endereço de email
+    $destinatario = "fatalzerax@gmail.com"; // Substitua pelo seu endereço de email
     $assunto = "Novo email do formulário do site";
 
     // Mensagem de email
