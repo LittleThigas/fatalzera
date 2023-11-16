656ada096f6dbc0c1144ac591329c000fdac033f
Thigas <147623461+LittleThigas@users.noreply.github.com>
2023-11-16 03:20:31 -0300

    gg

--- scripts/script.js
+++ scripts/script.js
@@ -14,10 +14,3 @@ function type() {
 
 type();
 
-function showPopup() {
-  var popup = document.getElementById("popup");
-  popup.style.display = "block";
-  setTimeout(function () {
-      popup.style.display = "none";
-  }, 5000);
-};
