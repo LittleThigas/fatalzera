ada332c1b7029750146df2ea2e37e2a4469b5abe
Thigas <147623461+LittleThigas@users.noreply.github.com>
2023-11-16 03:01:04 -0300

    att2

--- phps/formcontact.php
+++ phps/formcontact.php
@@ -13,44 +13,9 @@
 
     if(mail($para, $assunto, $corpo, $cabeca)) {
         if(mail($para, $assunto, $corpo, $cabeca)) {
-            echo "<script>
-                    var successMessage = document.createElement('div');
-                    successMessage.innerHTML = 'E-mail enviado com sucesso!';
-                    successMessage.className = 'alert success';
-                    document.body.appendChild(successMessage);
-                  </script>";
-        } else {
-            echo "<script>
-                    var errorMessage = document.createElement('div');
-                    errorMessage.innerHTML = 'Houve um erro ao enviar o email!';
-                    errorMessage.className = 'alert error';
-                    document.body.appendChild(errorMessage);
-                  </script>";
+            echo ("E-mail enviado com sucesso!");
+        }else{
+        echo ("Houve um erro ao enviar o email!");
         }
         
 ?>
-
-<style>
-    .alert {
-        position: fixed;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        padding: 15px;
-        border-radius: 10px;
-        color: #fff;
-        font-size: 18px;
-        font-weight: bold;
-        text-align: center;
-        z-index: 9999;
-        width: 300px; /* Ajuste a largura conforme necessário */
-    }
-
-    .success {
-        background-color: #4CAF50; /* Cor de fundo para sucesso */
-    }
-
-    .error {
-        background-color: #f44336; /* Cor de fundo para erro */
-    }
-</style>
